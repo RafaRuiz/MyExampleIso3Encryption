9592cc469734d488fba9954a459678f056618b3a
Rafael Ruiz Muñoz <raruizmu@gmail.com>
2020-08-31 13:58:24 +0200

    Initial project

---
+++ app/src/main/java/com/example/iso3/encryption/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.iso3.encryption
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
